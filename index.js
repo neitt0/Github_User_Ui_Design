860ad711821fef3504ead02c19b5d7ee901ed16d
Thien Nguyen <neiht0805@gmail.com>
2023-05-22 22:46:46 -0400

    add simple JS to #button to expand and collapse .user_info

---
+++ index.js
@@ -0,0 +1,6 @@
+const find = document.getElementById('find')
+
+find.addEventListener('click', () => {
+  const userInfo = document.getElementById('user_info')
+  userInfo.classList.toggle('hidden');
+})
